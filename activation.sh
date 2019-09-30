fd45a9959aff0ea6467911be669fb7ecc313bcdd
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-09-30 19:54:55 +0900

    universal activation

--- activation.sh
+++ activation.sh
@@ -1,5 +1,17 @@
 THIS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export PATH=$THIS/bin:$PATH
-export LD_LIBRARY_PATH=$THIS/lib:$LD_LIBRARY_PATH
-#export LD_LIBRARY_PATH=$THIS/lib64:$LD_LIBRARY_PATH
-#export C_INCLUDE_PATH=$THIS/include:$C_INCLUDE_PATH
+
+if [[ -d $THIS/bin ]]; then
+    export PATH=$THIS/bin:$PATH
+fi
+
+if [[ -d $THIS/lib ]]; then
+    export LD_LIBRARY_PATH=$THIS/lib:$LD_LIBRARY_PATH
+fi
+
+if [[ -d $THIS/lib64 ]]; then
+    export LD_LIBRARY_PATH=$THIS/lib64:$LD_LIBRARY_PATH
+fi
+
+if [[ -d $THIS/include ]]; then
+    export C_INCLUDE_PATH=$THIS/include:$C_INCLUDE_PATH
+fi
